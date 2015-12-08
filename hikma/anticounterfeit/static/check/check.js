f2daa7aaddd217cf6702bbb529a835e8c21423a0
akelsaman <ak.elsaman@gmail.com>
2015-12-08 18:47:07 +0200

    update views

--- hikma/anticounterfeit/static/check/check.js
+++ hikma/anticounterfeit/static/check/check.js
@@ -4,14 +4,24 @@
  * and open the template in the editor.
  */
 $(document).ready(function(){
-	$("#PharmacyState").load("http://127.0.0.1:8000/anticounterfeit/state/");
+	$("#Product").load("http://127.0.0.1:8000/anticounterfeit/product");
+
+	$("#PharmacyState").load("http://127.0.0.1:8000/anticounterfeit/state");
+	$("#PharmacyState").change(function() {
+		$("#PharmacyCity").load("http://127.0.0.1:8000/anticounterfeit/" + $("#PharmacyState").val() + "/city");
+	});
+	$("#PharmacyCity").change(function() {
+		$("#Pharmacy").load("http://127.0.0.1:8000/anticounterfeit/" + $("#PharmacyCity").val() + "/pharmacy");
+	});
+
+	$("#DoctorState").load("http://127.0.0.1:8000/anticounterfeit/state/");
+	$("#DoctorState").change(function() {
+		$("#DoctorCity").load("http://127.0.0.1:8000/anticounterfeit/" + $("#DoctorState").val() + "/city");
+	});
+	$("#DoctorCity").change(function() {
+		$("#Doctor").load("http://127.0.0.1:8000/anticounterfeit/" + $("#DoctorCity").val() + "/doctor");
+	});
 });
-function addRow(tableID){
-    var tr=$('<tr><td></td><td></td><td></td><td><button onclick="removeRow(this)">-</button></td></tr>').appendTo($("#"+tableID));
-    for(var i=0;i<3;i++){$(tr).children("td").eq(i).text($("select").eq(i).val());}
-    //var tr=$('<tr><td><button onclick="removeRow(this)">-</button></td></tr>').appendTo($("#"+tableID));
-    //for(var i=2;i>-1;i--){$(tr).prepend($("<td>" + $($("select")[i]).val() + "</td>"));}
-}
-function removeRow(removeRowButtonObject){
-    $(removeRowButtonObject).parents("tr").remove();
+function selectedState(){
+	$("#PharmacyState").val()
 }
